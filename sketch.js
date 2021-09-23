50348067e8bf56a021da944558ff1a0c5d18d406
Winry <winry.tian@gmail.com>
2021-09-23 12:10:35 -0700

    add version comments

--- sketch.js
+++ sketch.js
@@ -2,6 +2,27 @@
 author Winry
 @date 2021.09.23
 
+version comments draft
+    initial commit, blank project
+    version comments, here
+    use bezier function with mouse-controlled point
+    make wlerp
+    make wlerp 2D
+    bezier function to make mouse-controllable control point, cubic
+    implementing lerp from scratch: one dimension (number line)
+    quadratic lerp method: 3 lerp calls between 3 points
+    cubic lerp method
+        begin +end shape
+    bezier lerp mesh with t
+    refactor code for p5.Vector.lerp
+    make each vertex a particle with velocity, edges
+    advanced project:
+        drag and drop demo
+        add hover effect
+        transfer drag and drop code into p5-bezier
+    Cody's super advanced project:
+        <all caps> particle explosion upon contact!! <end all caps>
+
  */
 let font
 
