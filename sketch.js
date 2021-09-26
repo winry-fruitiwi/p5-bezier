a182e951e242771850d219dbbcc4e7dcb580f47e
Winry <winry.tian@gmail.com>
2021-09-26 10:37:40 -0700

    encapsulate quadratic bezier example. start cubic example

--- sketch.js
+++ sketch.js
@@ -25,72 +25,95 @@ version comments draft
 
  */
 let font
-let a, b, c, d, p1, p2, t
-
 
 function preload() {
     font = loadFont('fonts/Meiryo-01.ttf')
 }
 
+// fully encapsulates an example of a quadratic bezier
+class Quadratic_Bezier_Example {
+    constructor() {
+        this.a = new p5.Vector(0, height/2)
+        this.c = new p5.Vector(400, 300)
+        this.d = new p5.Vector(width, height/2)
+        this.p1 = new p5.Vector(random(50, 100), random(height));
+        this.p2 = new p5.Vector(random(width-100, width-50), random(height));
+        this.t = 1
+    }
+
+    draw() {
+        background(209, 80, 30)
+        // we can get the beziers now!
+        // bezier_example()
+
+        this.t = map(constrain(mouseX, this.p1.x, this.p2.x),
+            this.p1.x, this.p2.x,
+            0, 1);
+
+        stroke(0, 0, 100)
+
+        // where are the lerp points?
+        strokeWeight(8)
+        point(this.p1.x, this.p1.y)
+        point(this.p2.x, this.p2.y)
+
+        // We should see a line between the two points so that
+        // we can effectively test where our lerp should go.
+        strokeWeight(1)
+        line(this.p1.x, this.p1.y, this.p2.x, this.p2.y)
+
+        // Now we can draw the lerp in a different color, above
+        // everything else.
+        strokeWeight(8)
+        stroke(0, 80, 80);
+        let lerp_item = wlerp2D(this.p1, this.p2, this.t)
+        point(lerp_item.x, lerp_item.y)
+
+        // let's test wlerp! Print to the console whether
+        // my lerp is the same as p5.js lerp functions.
+        if (wlerp2D(this.p1, this.p2, this.t).x !== p5.Vector.lerp(this.p1, this.p2, this.t).x &&
+            wlerp2D(this.p1, this.p2, this.t).y !== p5.Vector.lerp(this.p1, this.p2, this.t).y) {
+            console.log("beep beep!")
+        }
+        // mouseX and mouseY doesn't exist before the program
+        // starts, and even if it did, we would not
+        // be able to update it every frame.
+        let b = new p5.Vector(mouseX, mouseY)
+
+        // Let's see what happens with the bezier example!
+        strokeWeight(3)
+        // The fill is way too bright!
+        noFill()
+        // where's the mouse?
+        point(mouseX, mouseY)
+        quadratic_bezier(this.a, b, this.d)
+    }
+
+
+}
+
+class Cubic_Bezier_Example {
+    constructor() {
+        // don't forget the this dots ♪ ♫ ♬
+    }
+
+    draw() {
+        
+    }
+}
+
+let quad_example
 
 function setup() {
     createCanvas(640, 360)
     colorMode(HSB, 360, 100, 100, 100)
-    a = new p5.Vector(0, height/2)
-    c = new p5.Vector(400, 300)
-    d = new p5.Vector(width, height/2)
-    p1 = new p5.Vector(random(50, 100), random(height));
-    p2 = new p5.Vector(random(width-100, width-50), random(height));
 
+    // quad_example = new Quadratic_Bezier_Example()
 }
 
 
 function draw() {
-    background(209, 80, 30)
-    // we can get the beziers now!
-    // bezier_example()
-
-    t = map(constrain(mouseX, p1.x, p2.x),
-        p1.x, p2.x,
-        0, 1);
-
-    stroke(0, 0, 100)
-
-    // where are the lerp points?
-    strokeWeight(8)
-    point(p1.x, p1.y)
-    point(p2.x, p2.y)
-
-    // We should see a line between the two points so that
-    // we can effectively test where our lerp should go.
-    strokeWeight(1)
-    line(p1.x, p1.y, p2.x, p2.y)
-
-    // Now we can draw the lerp in a different color, above
-    // everything else.
-    strokeWeight(8)
-    stroke(0, 80, 80);
-    let lerp_item = wlerp2D(p1, p2, t)
-    point(lerp_item.x, lerp_item.y)
-
-    // let's test wlerp! Print to the console whether
-    // my lerp is the same as p5.js lerp functions.
-    if (wlerp2D(p1, p2, t).x !== p5.Vector.lerp(p1, p2, t).x &&
-        wlerp2D(p1, p2, t).y !== p5.Vector.lerp(p1, p2, t).y) {
-        console.log("beep beep!")
-    }
-    // mouseX and mouseY doesn't exist before the program
-    // starts, and even if it did, we would not
-    // be able to update it every frame.
-    b = new p5.Vector(mouseX, mouseY)
-
-    // Let's see what happens with the bezier example!
-    strokeWeight(3)
-    // The fill is way too bright!
-    noFill()
-    // where's the mouse?
-    point(mouseX, mouseY)
-    quadratic_bezier(a, b, d)
+    // quad_example.draw()
 }
 
 
